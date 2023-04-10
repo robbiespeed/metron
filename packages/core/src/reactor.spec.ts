3a926f1ee8785febeb33c06e8f97e25d46529320
Robbie Speed <robert.y.speed@gmail.com>
2023-04-10 02:40:11 +0000

    wip

---
+++ packages/core/src/reactor.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+import { createReactor } from './reactor';
+import { createSensor } from './sensor';
+
+describe('Reactor', () => {
+  it('should create', () => {
+    expect(createReactor(() => {})).to.exist;
+  });
+  it('should react', () => {
+    const sensor = createSensor();
+    let count = 0;
+    createReactor(({ connect }) => {
+      connect(sensor);
+      count++;
+    });
+    sensor.send();
+    expect(count).to.equal(2);
+  });
+  it('should terminate', () => {
+    const sensor = createSensor();
+    let count = 0;
+    const clear = createReactor(({ connect }) => {
+      connect(sensor);
+      count++;
+    });
+    clear();
+    sensor.send();
+    expect(count).to.equal(1);
+  });
+});
--- packages/core/src/sensor.spec.ts
+++ packages/core/src/sensor.spec.ts
@@ -2,7 +2,19 @@ import { expect } from 'chai';
 import { describe } from 'mocha';
 import { createSensor } from './sensor';
 
-describe('RawSensor', () => {
+/**
+ * Only available in node if `--expose-gc` is passed to the process.
+ */
+declare const global: { gc?(): void };
+
+const garbageCollect =
+  global.gc &&
+  (async () => {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    global.gc!();
+  });
+
+describe('Sensor', () => {
   it('should create', () => {
     expect(createSensor()).to.exist;
   });
@@ -15,4 +27,42 @@ describe('RawSensor', () => {
     sensor.send();
     expect(count).to.equal(1);
   });
+  it('should send data', () => {
+    const sensor = createSensor<string>();
+    let message = '';
+    sensor.emitter((data) => {
+      message = data;
+    });
+    sensor.send('Hello');
+    expect(message).to.equal('Hello');
+  });
+  it('should terminate', () => {
+    const sensor = createSensor();
+    let count = 0;
+    const clear = sensor.emitter(() => {
+      count++;
+    });
+    clear();
+    sensor.send();
+    expect(count).to.equal(0);
+  });
+  if (garbageCollect) {
+    function createActiveWeakSensor() {
+      const sensor = createSensor();
+      let count = 0;
+      const clear = sensor.emitter(() => {
+        count++;
+      });
+      sensor.send();
+      // clear();
+      return new WeakRef(sensor);
+    }
+    it('should garbage collect', async () => {
+      const weakSensor = createActiveWeakSensor();
+      await garbageCollect();
+      expect(weakSensor.deref()).to.be.undefined;
+    });
+  } else {
+    it('should garbage collect');
+  }
 });
